e0f426d78082bf4eed51cd1f241929738ca39fdf
atsushi.arayashiki <atsushi.arayashiki415@gmail.com>
2021-03-17 12:10:38 +0900

    20210317_arayashiki

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="main">
+    <input type="text" v-model="user">
+    <button v-on:click="userclick">住所自動入力</button>
+    <p>Address :{{from}}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+async userclick(){
+const test = await axios.get(`https://apis.postcode-jp.com/api/v4/postcodes/0310023?&apiKey=1M9b5tZ0MrwrXMM5CWVODNawmTJfgAcs51O55cT`)
+}
+};
+const from = test.data;
+</script>

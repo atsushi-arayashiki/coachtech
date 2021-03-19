4950a06f5e225dec83b572fdd2725fc8d5a79b09
atsushi.arayashiki <atsushi.arayashiki415@gmail.com>
2021-03-20 05:09:22 +0900

    20210320_arayashiki

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,19 +9,19 @@
 <script>
 import axios from "axios";
 export default {
+  data() {
+    return{
+      from: "",
+      user: ""
+
+    }
+  },
  methods:{
   async userclick(){
-  const test = await axios.get(`https://apis.postcode-jp.com/api/v4/postcodes/{{from}}?&apiKey=1M9b5tZ0MrwrXMM5CWVODNawmTJfgAcs51O55cT`)
-  console.log(test);
+  const test = await axios.get(`https://apis.postcode-jp.com/api/v4/postcodes/${this.user}?&apiKey=1M9b5tZ0MrwrXMM5CWVODNawmTJfgAcs51O55cT`)
+  console.log(test.data[0].allAddress);
+  this.from = test.data[0].allAddress;
 }
 },
-computed: {
-  from(){
-    return this.test;
-  }
-  
 }
-
-};
-
 </script>
